20a398f9591e69d2cf41d5604a7f58f4ae4f7193
phusinh <nguyenvanphong1771999@mail.com>
2021-12-24 09:16:27 +0700

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+// https://semantic-ui.com/modules/dropdown.html#/definition
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "myprefix_topnav") {
+       x.className += " responsive";
+    } else {
+       x.className = "myprefix_topnav";
+    }
+ }
+//  console.log("aaaa")
